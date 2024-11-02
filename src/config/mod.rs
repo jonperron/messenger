3a3cd40f9930506bab0dd18e78c7da161c194c1b
Jonathan Perron <jonathan.perron@lumapps.com>
2024-11-02 08:39:55 +0100

    feat(config): add config struct

---
+++ src/config/mod.rs
@@ -0,0 +1,31 @@
+use serde::Deserialize;
+use std::fs;
+use std::path::Path;
+use serde_yaml;
+
+#[derive(Debug, Deserialize)]
+pub struct ServiceConfig {
+    pub name: String,
+    pub environment: String,
+    pub port: u16,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct TemplatesConfig {
+    pub path: String,
+    pub default_language: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub service: ServiceConfig,
+    pub templates: TemplatesConfig,
+}
+
+impl Config {
+    pub fn load_from_file<P: AsRef<Path>>(path: P) -> Result<Config, Box<dyn std::error::Error>> {
+        let content = fs::read_to_string(path)?;
+        let config: Config = serde_yaml::from_str(&content)?;
+        Ok(config)
+    }
+}
